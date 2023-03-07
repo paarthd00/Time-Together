8d948fdfb2067a5a2d6ccd95126b2e13776d7696
paarthd00 <paarthcode@gmail.com>
2023-03-07 14:46:23 -0800

    first-commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Time Together</h1>  
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
